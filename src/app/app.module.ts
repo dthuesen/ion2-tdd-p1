eca494c23489632527ffb6900e14aa34112260ea
Detlef von der Thuesen <dthuesen@gmx.de>
2017-03-27 20:38:01 +0200

    Add StatusBar + SplashSceen providers to app.module. Update TypeScript from 2.0.9 to 2.2.2 (caused incompatibility with Jasmine). Add <ion-list> and <ion-item> to product view template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
 import { MyApp } from './app.component';
 import { ProductPage } from '../pages/product/product';
 
@@ -20,6 +23,8 @@ import { Products } from '../providers/products';
   ],
   providers: [
     {provide: ErrorHandler, useClass: IonicErrorHandler},
+    SplashScreen,
+    StatusBar,
     Products
   ]
 })
--- src/pages/product/product.ts
+++ src/pages/product/product.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Products } from '../../providers/products';
 
-/*
-  Generated class for the Product page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-product',
   templateUrl: 'product.html'
 })
 export class ProductPage {
 
-  constructor(public navCtrl: NavController) {}
+  constructor(
+    public navCtrl: NavController, 
+    public productsService: Products) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProductPage');
